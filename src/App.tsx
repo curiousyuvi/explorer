aa5d8a4cb357de4fa495c56a8fe0fa2642c28716
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-26 18:34:50 +0530

    added routing and created Home and SearchResults components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import SearchResults from "./Pages/SearchResults";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Home />} path="/"></Route>
+        <Route element={<SearchResults />} path="/:query"></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
