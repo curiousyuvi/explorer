13b9ce707816236a46b59f3a444f328bbb42442a
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-28 03:21:22 +0530

    created SearchNavbar component

---
+++ src/Components/SearchNavbar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import "../Styles/SearchNavbar.css";
+import SearchInput from "./SearchInput";
+
+const SearchNavbar = () => {
+  const params = useParams();
+  const [query, setQuery] = useState<string>(params.query || "");
+  const navigator = useNavigate();
+  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setQuery(e.target.value);
+  };
+  const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    navigator(`/${query}`);
+  };
+  return (
+    <div className="search-navbar">
+      <span />
+      <form onSubmit={handleFormSubmit}>
+        <SearchInput
+          onChange={handleInputChange}
+          value={query}
+          placeholder="Search for products"
+        />
+      </form>
+      <Link to="/">
+        <img src="/logo.png" alt="EXPLORER" className="logo" />
+      </Link>
+    </div>
+  );
+};
+
+export default SearchNavbar;
--- src/Pages/SearchResults.tsx
+++ src/Pages/SearchResults.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import SearchNavbar from "../Components/SearchNavbar";
+import "../Styles/SearchResults.css";
 
 const SearchResults = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search-results">
+      <SearchNavbar />
+    </div>
+  );
 };
 
 export default SearchResults;
