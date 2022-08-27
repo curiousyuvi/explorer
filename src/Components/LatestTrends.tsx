f7dff52ccaef6270d74dcf0a03bb49c14521c6be
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-28 01:12:59 +0530

    created LatestTrends component

--- src/Components/LatestTrends.tsx
+++ src/Components/LatestTrends.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Product } from "../Interfaces/Product";
+import { getProducts } from "../Services/fake-store-api";
+import LatestTrendsItem from "./LatestTrendsItem";
+import "../Styles/LatestTrends.css";
 
 const LatestTrends = () => {
-  return <div>LatestTrends</div>;
+  const [items, setItems] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const loadItems = async () => {
+      setItems(await getProducts(10));
+    };
+
+    loadItems();
+  }, []);
+
+  return (
+    <div className="latest-trends_wrapper">
+      <h1>Latest Trends</h1>
+      <div className="latest-trends_list">
+        {items.map((item) => {
+          return <LatestTrendsItem product={item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default LatestTrends;
--- src/Components/SearchHelper.tsx
+++ src/Components/SearchHelper.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import LatestTrends from "./LatestTrends";
+import PopularSuggestions from "./PopularSuggestions";
+import "../Styles/SearchHelper.css";
 
 const SearchHelper = () => {
-  return <div>SearchHelper</div>;
+  return (
+    <div className="search-helper-wrapper">
+      <LatestTrends />
+      <PopularSuggestions />
+    </div>
+  );
 };
 
 export default SearchHelper;
