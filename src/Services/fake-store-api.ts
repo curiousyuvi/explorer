946ef6799eab65e3154a587cf5ac00d6f6e6f39c
Yuvraj Singh <ysgaur9919@gmail.com>
2022-08-28 15:43:46 +0530

    added random price and rating to Product

--- src/Services/fake-store-api.ts
+++ src/Services/fake-store-api.ts
@@ -5,6 +5,17 @@ const fake_store_api_endpoint = "https://fakestoreapi.com"
 
 const fs_api_axios_instance = axios.create({ baseURL: fake_store_api_endpoint })
 
+
+const generateRandomPrice = () => Math.floor(Math.random() * 4000).toString()
+const generateRandomRating = () => Math.floor(Math.random() * 6)
+const generateRandomRatingCount = () => Math.floor(Math.random() * 300)
+
+
+
+const generateLargeAmountOfProducts = (products: Product[]) => {
+    return [...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } }), ...products.map((product: Product) => { return { id: product.id, title: product.title, price: generateRandomPrice(), image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } })]
+}
+
 const getProducts: (limit?: number) => Promise<Product[]> = async (limit) => {
     try {
         const response = await fs_api_axios_instance({
@@ -12,7 +23,11 @@ const getProducts: (limit?: number) => Promise<Product[]> = async (limit) => {
         })
 
         if (response.status === 200 && response.data)
-            return response.data.filter((product: Product) => { return { id: product.id, title: product.title, price: product.price, image: product.image } })
+            if (limit)
+                return response.data.map((product: Product) => { return { id: product.id, title: product.title, price: product.price, image: product.image, rating: generateRandomRating(), ratingCount: generateRandomRatingCount() } })
+            else {
+                return generateLargeAmountOfProducts(response.data)
+            }
         else
             return []
     } catch (err) {
